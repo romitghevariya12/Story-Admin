4724134ae6b35bed87401d278cdb6cbb88a58e24
romitghevariya12 <romitghevariya123216@gmail.com>
2022-01-29 12:33:37 +0530

    navigation

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Logo from '../logo.svg';
 
 export default function Navigation() {
   return (
-    <div className="container-fluid hader">
-        <a href="#">Logo Here</a>
-    </div>
+    <>
+    <nav className="navbar navbarHeader">
+      <a className="navbar-brand" href="#"><img src={Logo} /></a>
+      <button type="submit" className="btn">Log out</button>
+    </nav>
+
+    <div className=""></div>
+    </>
   );
 }
