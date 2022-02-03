f17241dc78d189b41f1684910275842fdf6ab283
romitghevariya12 <romitghevairya123216@gmail.com>
2022-02-03 22:07:17 +0530

    add categogry model

--- src/Components/Collection.js
+++ src/Components/Collection.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import UploadModel from './Uploadmodel'
+import CategoryMOdel from './CategoryModel'
 import Sidemenu from './Siddebar'
 import Deleteicon from '../delete-icon.svg'
 
 export default function Collection() {
   return (
-    <div className="d-lg-flex trending">
+    <div className="d-lg-flex collection trending">
       <Sidemenu />
       <div className="page-trending">
         <div className="navbar title">
           <h3>Collection</h3>
-          <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#bannerupload" data-bs-whatever="@mdo">Add Trending Photos</button>
-          <UploadModel />
+          <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#bannerupload" data-bs-whatever="@mdo">Add Category Name</button>
+          <CategoryMOdel />
         </div>
         <div className="row">
           <div className="banner-photo">
             <div className="photo">
-            <a className="icon" href="#"><img src={Deleteicon} /></a>
+              <a className="icon" href="#"><img src={Deleteicon} /></a>
             </div>
           </div>
         </div>
