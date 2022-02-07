8add7f755491c491df9ebf10881be20e953d115d
romitghevariya12 <romitghevairya123216@gmail.com>
2022-02-07 09:08:11 +0530

    collection commit

--- src/Components/CategoryModel.js
+++ src/Components/CategoryModel.js
@@ -4,20 +4,24 @@ import './CategoryModel.css';
 export default function CategoryModel() {
   return (
     <div className="category-model">
-      <div class="modal fade" id="bannerupload" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Add Category Name</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      <div className="modal fade" id="bannerupload" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">Add Category Name</h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
+            <div className="modal-body">
               <label for="exampleInputEmail1" className="form-label">Email address</label>
-              <input type="email" className="form-control" aria-describedby="emailHelp" placeholder="Enter your email" />
+              <input className="form-control" aria-describedby="emailHelp" placeholder="Enter your Category Name" />
+                <div className="form-check">
+                  <input className="form-check-input" type="checkbox" role="switch" id="listnewcategory" />
+                  <label className="form-check-label" for="listnewcategory">New Category</label>
+                </div>
             </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-2" data-bs-dismiss="modal">Cancel</button>
-              <button type="button" class="btn btn-3">Upload</button>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-2" data-bs-dismiss="modal">Cancel</button>
+              <button type="button" className="btn btn-3">Save</button>
             </div>
           </div>
         </div>
--- src/Components/Collection.js
+++ src/Components/Collection.js
@@ -2,22 +2,23 @@ import React from 'react';
 import './Collection.css';
 import CategoryModel from './CategoryModel'
 import Sidemenu from './Sidebar'
-import Deleteicon from '../delete-icon.svg'
+import Editicon from '../edit-icon.svg'
 
 export default function Collection() {
   return (
-    <div className="d-lg-flex collection trending">
+    <div className="d-lg-flex collection">
       <Sidemenu />
-      <div className="page-trending">
+      <div className="page-collection">
         <div className="navbar title">
           <h3>Collection</h3>
           <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#bannerupload" data-bs-whatever="@mdo">Add Category Name</button>
         </div>
         <CategoryModel />
         <div className="row">
-          <div className="banner-photo">
-            <div className="photo">
-              <a className="icon" href="#"><img src={Deleteicon} /></a> 
+          <div className="category-list">
+            <div className="category-name">
+              Valentine's Day
+              <a className="icon" href="#"><img src={Editicon} /></a> 
             </div>
           </div>
         </div>
